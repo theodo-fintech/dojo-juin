5bbd473486af9dc7a435fa28e7425c69772f02c3
Brice Conraux <bricec@sipios.com>
2024-04-29 20:28:55 +0200

    added getUserLevel function in service

--- dojo-front/dojo-front/src/app/home/services/user.service.ts
+++ dojo-front/dojo-front/src/app/home/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of, delay } from 'rxjs';
 import { UserAmount } from '../interfaces/amount.interface';
 import { Asset } from '../interfaces/asset.interface';
 import { Score } from 'src/app/navbar/interfaces/score.interface';
@@ -19,6 +19,10 @@ export class UserService {
     return this.http
       .get<UserAmount>('/bff/user/amount')
       .pipe(map((response) => response.amount));
+  }
+
+  getUserLevel(): Observable<number> {
+    return of(2).pipe(delay(1000));
   }
 
   fetchUserRealties(): Observable<Asset[]> {
