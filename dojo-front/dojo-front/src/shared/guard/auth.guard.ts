a585a315a1d0214c6d788eb44640a1c761ab8fa6
merranleo <leo.merran@ext.bpifrance.fr>
2024-05-02 13:42:02 +0200

    feat(dojo): update angular cli to 16

--- dojo-front/dojo-front/src/shared/guard/auth.guard.ts
+++ dojo-front/dojo-front/src/shared/guard/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { AuthService } from '../../app/login/services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): Observable<boolean> {
