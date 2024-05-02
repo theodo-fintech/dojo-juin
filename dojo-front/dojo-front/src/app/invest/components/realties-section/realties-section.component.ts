4216b54a48703594a5537035c9f7efecbaed6f9b
merranleo <leo.merran@ext.bpifrance.fr>
2024-05-02 13:39:40 +0200

    feat(dojo): update angular material to 15

--- dojo-front/dojo-front/src/app/invest/components/realties-section/realties-section.component.ts
+++ dojo-front/dojo-front/src/app/invest/components/realties-section/realties-section.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {PageEvent} from '@angular/material/paginator';
+import {LegacyPageEvent as PageEvent} from '@angular/material/legacy-paginator';
 import {Realty} from 'src/shared/interface/realty';
 
 @Component({
