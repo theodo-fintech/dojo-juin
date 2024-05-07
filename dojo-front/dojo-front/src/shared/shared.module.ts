9dc14b865e5d8a72e9043323579e472a2d52fc00
merranleo <leo.merran@ext.bpifrance.fr>
2024-05-07 22:42:09 +0200

    feat(dojo): update shared imports

--- dojo-front/dojo-front/src/shared/shared.module.ts
+++ dojo-front/dojo-front/src/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+
 import { RealtyCardComponent } from './component/realty-card/realty-card.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [RealtyCardComponent],
